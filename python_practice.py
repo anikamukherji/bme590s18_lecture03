07f0c946f1b2428fc29b9cb8368ea412647f9769
anika-mukherji <anika.mukherji@duke.edu>
2018-02-01 12:10:23 -0500

    a few stylistic, commenting, and bounds checking changes

--- python_practice.py
+++ python_practice.py
@@ -3,8 +3,10 @@ import csv
 def main():
     files = return_all_csv(".", ['mlp6'])
     write_data(files, file_type='csv')
-    print("{} people used camel case for their team names".format(camel_case_count()))
-    print("{} people used spaces in their team names".format(check_spaces_all_teamnames()))
+    print("{} people used CamelCase for "
+            "their team names".format(camel_case_count()))
+    print("{} people used spaces in their "
+            "team names".format(check_spaces_all_teamnames()))
     write_data(files, file_type='json')
 
 ####################
@@ -13,30 +15,39 @@ def main():
 
 def write_data(files, file_type):
     """
-    Concatenates data from files into single file
+    files = [String]
+    file_type = String
+
     Calls helper functions depending on file_type passed in
+    csv = concatenates data from files into single csv file 
+    json = creates a json file for each file in files array 
     """
     if file_type == 'json':
-        write_json_file(files)
+        write_json_files(files)
 
     elif file_type == 'csv':
         write_csv_file(files)
 
-def write_json_file(files):
+    else:
+        print("Invalid file type passed in")
+
+def write_json_files(files):
     """
-    Concatenates all files given into a single 
-    json file 
+    files = [String]
+
+    Creates a json file for each file in files array
     """
     import json
     import csv
-    fields = ("first_name", "last_name", "net_id", "github_username", "team_name")
+    fields = ("first_name", "last_name", "net_id", \
+            "github_username", "team_name")
 
     for aFile in files:
         with open(aFile, 'r') as csv_file:
             dict_reader = csv.DictReader(csv_file, fields)
             for row in dict_reader:
-                # make sure all data is present
-                if len(row) < 5:
+                # check to make sure row has all appropriate data
+                if len(row) != 5:
                     break
                 net_id = row["net_id"].strip()
                 json_file = open("./{}.json".format(net_id), 'w')
@@ -47,9 +58,9 @@ def write_json_file(files):
 
 def write_csv_file(files):
     """
-    Concatenates all files given into a single 
-    csv file with entries from each file separated 
-    by a newline
+    files = [String]
+
+    Concatenates data from files into single csv file
     """
     outfile = open("./everyone.csv",'w')
     for aFile in files:
@@ -66,9 +77,12 @@ def write_csv_file(files):
 
 def return_all_csv(filepath, exception=None):
     """
+    filepath = String
+    exception = String?
+
     Returns all csv files for a given file path
     Except for files specified in the optional 
-    exception parameter (pass in [String])
+    exception parameter 
     """
     import glob as glob
     all_csv = glob.glob('{}/*.csv'.format(filepath))
@@ -82,20 +96,20 @@ def return_all_csv(filepath, exception=None):
 def camel_case_count():
     """
     Counts how many of the files given
-    adhere to CamelCase for their last entry
-    (using the everyone.csv file)
+    in everyone.csv file adhere to CamelCase 
+    for their last entry
     """
     import csv
     counter = 0
     with open('./everyone.csv', 'r') as f:
         reader = csv.reader(f)
         for row in reader:
-            # check to make sure row has all relevant data
-            if len(row) < 5:
+            # check to make sure row has appropriate data
+            if len(row) != 5:
                 continue
             team_name = row[-1].strip()
             if contains_space(team_name):
-                print("{} used a space in their team name!\n".format(row[0]))
+                continue
             elif uses_camel_case(team_name):
                 counter += 1
     return counter
@@ -104,16 +118,16 @@ def camel_case_count():
 def check_spaces_all_teamnames():
     """
     Counts how many of the files given
-    used a space in their team name
-    (using the everyone.csv file)
+    in everyone.csv file used a space 
+    in their team name
     """
     import csv
     counter = 0
     with open('./everyone.csv', 'r') as f:
         reader = csv.reader(f)
         for row in reader:
-            # check to make sure row has all relevant data
-            if len(row) < 5:
+            # check to make sure row has appropriate data
+            if len(row) != 5:
                 continue
             team_name = row[-1].strip()
             if contains_space(team_name):
@@ -126,7 +140,8 @@ def check_spaces_all_teamnames():
 
 def uses_camel_case(name):
     """
-    Takes in string
+    name = String
+
     Returns True if name contains at least 1 uppercase
     and 1 lowercase value, and only letters are present
     Returns False otherwise
@@ -140,7 +155,8 @@ def uses_camel_case(name):
 
 def contains_space(name):
     """
-    Takes in string
+    name = String
+
     Returns True if the string contains a space
     Returns False otherwise
     """
